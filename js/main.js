35558709a5f03efb40b53ea83ee4ff343cb6a88c
Alex Kuvaldin <kuvaldin@5th.ru>
2021-08-22 12:53:31 +0700

    page revision

--- js/main.js
+++ js/main.js
@@ -3,17 +3,25 @@ $(document).ready(function () {
     let counterUp = $(".counter__up");
     let counterDown = $(".counter__down");
     let floorPath = $('.home-image path');
+    let modal = $('.modal');
+    let modalClose = $('.modal__close-button');
+    let viewFlatsButton = $('.view-flats');
+
     floorPath.on("mouseover", function () {
         currentFloor = $(this).attr('data-floor');
         floorPath.removeClass('current-floor');
-        $('.counter__text').text(currentFloor);
+        $('.counter').text(currentFloor);
     })
+
+    floorPath.on('click',toggleModal );
+    modalClose.on('click', toggleModal);
+    viewFlatsButton.on('click', toggleModal);
 
     counterUp.on('click', function () {
         if (currentFloor < 18) {
             currentFloor++;
             usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-            $('.counter__text').text(usCurrentFloor);
+            $('.counter').text(usCurrentFloor);
             floorPath.removeClass('current-floor');
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
@@ -23,9 +31,13 @@ $(document).ready(function () {
         if (currentFloor > 2) {
             currentFloor--;
             usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-            $('.counter__text').text(usCurrentFloor);
+            $('.counter').text(usCurrentFloor);
             floorPath.removeClass('current-floor');
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
     })
+
+    function toggleModal() {
+        modal.toggleClass('is-open');
+    }
 });
